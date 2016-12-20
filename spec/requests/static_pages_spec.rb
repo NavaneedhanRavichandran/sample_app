598a023dcd30d25e2a934e2820c200b9d19f0a32
rnavaneedhan <navaneedhan.cit@gmail.com>
2016-12-20 11:12:03 +0530

    Add microposts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,6 +9,23 @@ describe "StaticPages" do
     		before{ visit root_path }
     		it { should have_content('Sample App') }
     		it { should have_title('Ruby on Rails Tutorial Sample App | Home') }
+
+        describe "for signed-in users" do
+          let(:user) { FactoryGirl.create(:user) }
+
+          before do
+            FactoryGirl.create(:micropost, user: user, content: "alpha")
+            FactoryGirl.create(:micropost, user: user, content: "beta")
+            sign_in user
+            visit root_path
+          end
+
+          it "should render user's feed" do
+            user.feed.each do |item|
+              expect(page).to have_selector("li##{item.id}", text: item.content)
+            end
+          end
+        end
   		end
   		
   		describe "Help page" do
